7ff98cf1f99c507f8c7fa3a029fa26141449625b
Inby <hastakpatel19@gmail.com>
2022-01-20 14:09:58 +0530

    Updates

---
+++ precache-manifest.709b4d4b8a644ae3aaccbb2fb63a9951.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a233a827cf3ba264a1324c33ba1d449",
+    "url": "/superChat/index.html"
+  },
+  {
+    "revision": "d7a3fef98f0514951f81",
+    "url": "/superChat/static/css/2.3f4110c8.chunk.css"
+  },
+  {
+    "revision": "6429658b6251e2b2ae0b",
+    "url": "/superChat/static/css/main.0b0e1f3a.chunk.css"
+  },
+  {
+    "revision": "d7a3fef98f0514951f81",
+    "url": "/superChat/static/js/2.6df8b0a4.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/superChat/static/js/2.6df8b0a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6429658b6251e2b2ae0b",
+    "url": "/superChat/static/js/main.5913338f.chunk.js"
+  },
+  {
+    "revision": "93f6dc9b79962c78a6d3",
+    "url": "/superChat/static/js/runtime-main.cf94d2e4.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/superChat/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/superChat/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/superChat/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/superChat/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/superChat/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "835148a5a421e9bc1dbe4b4c3fe1cdf8",
+    "url": "/superChat/static/media/logo.835148a5.png"
+  }
+]);
